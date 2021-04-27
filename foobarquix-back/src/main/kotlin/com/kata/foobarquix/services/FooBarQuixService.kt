f5ec83f3b6d5a13e703ff1107b1fdb7497145f4d
omar <elbouhassaniomar@gmail.com>
2021-04-27 19:59:13 +0200

    [BACK] finalize convertNumber

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -4,23 +4,28 @@ import org.springframework.stereotype.Component
 
 @Component
 class FooBarQuixService {
-    val replaceMap = mapOf(3 to "Foo")
+    val replaceMap = mapOf('3' to "Foo", '5' to "Bar", '7' to "Quix").withDefault { "" }
 
- 
+
     fun convertNumber(inputNumber: Int): String {
-        if(inputNumber == 1) return "1"
-        return checkContain(inputNumber.toString()) + checkDivise(inputNumber);
-       
+        if (inputNumber == 1) return "1"
+        val result = appendIfDivisible(inputNumber) + replaceOccurence(inputNumber.toString());
+        return if (result != "") result else inputNumber.toString();
     }
-    
-    fun checkContain(numberAsString: String) : String {
-        if(numberAsString.contains("3")) return replaceMap.getValue(3);
-        return "";
+
+    fun replaceOccurence(numberAsString: String): String {
+        val result = StringBuilder()
+        for (i in 0..numberAsString.length - 1) {
+            result.append(replaceMap.getValue(numberAsString[i]));
+        }
+        return result.toString();
 
     }
 
-    fun checkDivise(inputNumber: Int) : String{
-        if(inputNumber%3 == 0) return replaceMap.getValue(3); 
-        return "";
+    fun appendIfDivisible(inputNumber: Int): String {
+        val result = StringBuilder()
+        if (inputNumber % 3 == 0) result.append(replaceMap.getValue('3'));
+        if (inputNumber % 5 == 0) result.append(replaceMap.getValue('5'));
+        return result.toString();
     }
 }
--- foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixServiceTest.kt
+++ foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixServiceTest.kt
@@ -1,10 +1,8 @@
-
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DynamicTest;
 import com.kata.foobarquix.services.FooBarQuixService
-
 
 
 class FooBarQuixApplicationTests {
@@ -13,13 +11,21 @@ class FooBarQuixApplicationTests {
 
     @TestFactory
     fun tetConvert() = listOf(
-        1 to "1",
-        3 to "FooFoo")
-        .map { (input, expected) ->
-            DynamicTest.dynamicTest("when I convert $input then I get $expected") {
-                assertEquals(expected, service.convertNumber(input))
+            1 to "1",
+            3 to "FooFoo",
+            5 to "BarBar",
+            7 to "Quix",
+            9 to "Foo",
+            51 to "FooBar",
+            53 to "BarFoo",
+            33 to "FooFooFoo",
+            27 to "FooQuix",
+            15 to "FooBarBar",
+            11 to "11",
+            16 to "16")
+            .map { (input, expected) ->
+                DynamicTest.dynamicTest("when I convert $input then I get $expected") {
+                    assertEquals(expected, service.convertNumber(input))
+                }
             }
-        }
-
-
 }
